a7efcc82c69545b9063a17a1b56be32ddaa3b36b
maikeip.github.io <hajilk@yeah.net>
2023-10-06 20:24:31 +0800

    1.3添加配置文件

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask
+import os
+
+from flask import Flask, current_app
+
+from configs import config
+from utils import setup_log
 
 app = Flask(__name__)
+
+# 尝试从本地环境中读取
+config_name = os.getenv("FLASK_CONFIG", "development")
+app.config.from_object(config[config_name])
+setup_log(config_name)
 
 
 @app.route("/")
 def hello_world():  # put application's code here
+    current_app.logger.debug("debug")
+    current_app.logger.error("error")
     return "Hello World!"
-
-
-if __name__ == "__main__":
-    app.run()
---
+++ configs.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# filename:configs.py
+import logging
+import os
+
+
+class BaseConfig:
+    SECRET_KEY = os.getenv("SECRET_KEY", "dev key")
+
+    # mysql 数据库的配置信息
+    SQLALCHEMY_DATABASE_URI = ""
+
+    # redis配置
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
+    # 默认日志等级
+    LOG_LEVEL = logging.DEBUG
+
+
+class DevelopmentConfig(BaseConfig):
+    """开发配置"""
+
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:mysql@127.0.0.1:3306/qingdengbbs"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class TestingConfig(BaseConfig):
+    """测试配置"""
+
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"  # 内存数据库
+
+
+class ProductionConfig(BaseConfig):
+    """生成环境配置"""
+
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:mysql@127.0.0.1:3306/qingdengbbs"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    """生产模式下的配置"""
+    LOG_LEVEL = logging.ERROR
+
+
+config = {
+    "development": DevelopmentConfig,
+    "testing": TestingConfig,
+    "production": ProductionConfig,
+}
