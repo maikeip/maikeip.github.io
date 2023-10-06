ae6e0d012f2d81277af764753a24a90662720361
maikeip.github.io <hajilk@yeah.net>
2023-10-06 21:05:44 +0800

    1.5工厂函数创建程序实例

---
+++ qing/__init__.py
@@ -0,0 +1,33 @@
+import os
+
+from flask import Flask
+
+from configs import config
+from qing.com.utils import setup_log
+from qing.extensions import db, migrate
+from qing.views.index import index_bp
+
+
+# 使用工厂函数创建程序实例
+def create_app(config_name=None):
+    app = Flask("qdbbs")
+    if not config_name:
+        config_name = os.getenv("FLASK_CONFIG", "development")
+    app.config.from_object(config[config_name])
+    setup_log(config_name)
+
+    # 使用函数注册插件
+    register_extensions(app)
+
+    register_blueprint(app)
+
+    return app
+
+
+def register_extensions(app: Flask):
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+
+def register_blueprint(app: Flask):
+    app.register_blueprint(index_bp)
--- qing/extensions.py
+++ qing/extensions.py
@@ -1,4 +1,6 @@
 # filename:extensions.py
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+migrate = Migrate()
